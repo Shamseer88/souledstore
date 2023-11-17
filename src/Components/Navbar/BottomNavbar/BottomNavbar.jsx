dbae23dd13a8de8f5bc2469a4e5686a7a8a45f9f
Shamseer88 <alishamseer@gmail.com>
2023-11-17 15:50:06 +0530

    bottom nav designed

--- src/Components/Navbar/BottomNavbar/BottomNavbar.jsx
+++ src/Components/Navbar/BottomNavbar/BottomNavbar.jsx
@@ -1,5 +1,27 @@
 import "./BottomNavbar.css";
+import { SlHandbag, SlHeart, SlUser } from "react-icons/sl";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function BottomNavbar() {
-  return <div className="bottom-nav">BottomNavbar</div>;
+  return (
+    <nav className="bottom-nav">
+      <div className="left">
+        <p>Shirt</p>
+        <p>Jeans</p>
+        <p>Jogger</p>
+        <p>Pants</p>
+        <RxHamburgerMenu
+          size={20}
+          color="#58595b"
+          className="hamburger-mewnu"
+        />
+      </div>
+      <div className="right">
+        <input type="text" placeholder="Search..." />
+        <SlUser size={20} color="#58595b" />
+        <SlHeart size={20} color="#58595b" />
+        <SlHandbag size={20} color="#58595b" />
+      </div>
+    </nav>
+  );
 }
